9dcdd7ab6ce0ab9370ebcdeccf2a75775b2c8d50
Cameron Collver <crcollver@gmail.com>
2021-02-28 12:47:54 -0800

    fix: removed once listener when retrieving messages

--- src/components/Room/Messaging.vue
+++ src/components/Room/Messaging.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <div v-if="allMessages.length" class="h-48 overflow-y-auto">
+    <div class="h-48 overflow-y-auto">
       <message-bubble
         v-for="message in allMessages"
         :key="message.id"
         :message="message"
       />
     </div>
-    <div v-if="loading">Loading messages...</div>
-    <div v-if="!allMessages.length && !loading" class="h-36">
+    <div v-if="!allMessages.length" class="h-36">
       No messages to display yet!
     </div>
-    <div v-if="!loading && error">{{ error }}</div>
     <message-box />
   </div>
 </template>
@@ -31,36 +29,18 @@ export default {
     MessageBubble,
   },
   setup() {
-    const loading = ref<boolean>(true);
-    const error = ref<string>("");
     const allMessages = ref<Message[]>([]);
 
     const route = useRoute();
     const roomRef = messagesRef.child(route.params.id.toString());
 
-    /**
-     * Fetch all messages initially, then setup listener
-     * Firebase guarantees that "value" events always run after "child_" events
-     */
-    const createListener = async () => {
-      try {
-        roomRef.on("child_added", (snapshot) => {
-          allMessages.value.push({ id: snapshot.key, ...snapshot.val() });
-        });
-        roomRef.once("value", () => {
-          loading.value = false;
-        });
-      } catch (err) {
-        console.error(err);
-        error.value = "Something went wrong setting up messages.";
-      }
-    };
+    // add a setTimeout to counter the brief time allMessages is empty
+    roomRef.on("child_added", (snapshot) => {
+      allMessages.value.push({ id: snapshot.key, ...snapshot.val() });
+    });
 
-    createListener();
     onBeforeUnmount(() => roomRef.off());
     return {
-      loading,
-      error,
       allMessages,
     };
   },
